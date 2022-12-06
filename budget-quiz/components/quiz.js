915e6af3ae7631b41730a487a812095bdf10c0ed
Lincoln Swaine-Moore <lswainemoore@gmail.com>
2022-12-06 05:06:00 -0500

    todos

--- budget-quiz/components/quiz.js
+++ budget-quiz/components/quiz.js
@@ -409,17 +409,3 @@ class Question extends React.Component {
 }
 
 export default Quiz;
-
-
-// TODO
-// functionality
-// ~ make a visualization after you select answer (i'm thinking bars drop down from buttons)
-// - finished state
-// - hints (e.g. 2B is X twinkies...oh you meant a helpful hint??)
-// - stats (e.g. you scored better than X% of users, and 80% of Representatives*  *probably)
-// - selection of questions to toss out really hard things
-// - difficulty level to affect ratios used for ^
-// - links out to relevant budget functions
-// styling
-// - buttons
-// - everything else
